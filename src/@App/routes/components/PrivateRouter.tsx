6245a9b1bfa38f52f7a6eed38b91cea827f738e0
phamtuan19 <phamtuan19hd@gmail.com>
2023-10-07 15:22:04 +0700

    refactor private route

--- src/@App/routes/components/PrivateRouter.tsx
+++ src/@App/routes/components/PrivateRouter.tsx
@@ -1,10 +1,14 @@
+import ROLE from '@App/configs/role';
+import { useAuth } from '@App/redux/slices/auth.slice';
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 export default function PrivateRouter(props: { children: React.ReactNode }) {
-   //    const { isAuhthentication, isInitialized } = useAuth();
-   //    if (!isAuhthentication && isInitialized) {
-   //       return <Navigate to="/signin" replace />;
-   //    }
+   const { auth } = useAuth();
+
+   if (!auth.isAuhthentication && auth.isInitialized && auth.userPermission === ROLE[1]) {
+      return <Navigate to="/" replace />;
+   }
+
    return props.children;
 }
