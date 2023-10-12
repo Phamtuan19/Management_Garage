5fa5cc932ce0173e65f2751912c200de854340a1
tahhnn <thanhphuongmao4@gmail.com>
2023-10-12 11:00:18 +0700

    add header component (responsive)

--- src/@App/component/Layout/CommonLayout/components/Header/index.tsx
+++ src/@App/component/Layout/CommonLayout/components/Header/index.tsx
@@ -1,9 +1,103 @@
-import { Stack, styled } from "@mui/material";
-
+import { Box, Button, Container, IconButton, Menu, MenuItem, Stack, Toolbar, Typography, styled } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 function CommonHeader() {
+   const pages = ['Khóa học', 'Lộ trình', 'Bài viết'];
+   const [isLogin, setLogin] = useState(false);
    return (
       <CommonHeaderNav>
-         {/* <Stack padding={24} spacing={2}></Stack> */}
+         <Container maxWidth={'xl'}>
+            <Toolbar disableGutters>
+               <Typography
+                  variant={'h6'}
+                  component="a"
+                  href="#"
+                  sx={{
+                     mr: 2,
+                     display: { xs: 'none', md: 'flex' },
+                     fontFamily: 'monospace',
+                     fontWeight: 700,
+                     letterSpacing: '.3rem',
+                     color: 'inherit',
+                     textDecoration: 'none',
+                  }}
+               >
+                  Elearning
+               </Typography>
+               <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                  <IconButton size="large">
+                     <MenuIcon />
+                  </IconButton>
+               </Box>
+               <Typography
+                  variant="h5"
+                  noWrap
+                  component="a"
+                  href="#app-bar-with-responsive-menu"
+                  sx={{
+                     mr: 2,
+                     display: { xs: 'flex', md: 'none' },
+                     flexGrow: 1,
+                     fontFamily: 'monospace',
+                     fontWeight: 700,
+                     letterSpacing: '.3rem',
+                     color: 'inherit',
+                     textDecoration: 'none',
+                     justifyContent: 'center',
+                  }}
+               >
+                  Elearning
+               </Typography>
+
+               <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                  {pages.map((page) => (
+                     <Button
+                        key={page}
+                        sx={{
+                           my: 0,
+                           color: '#000',
+                           backgroundColor: '#fff',
+                           display: 'block',
+                           ':hover': { backgroundColor: '#fff' },
+                           mx: '5px',
+                        }}
+                     >
+                        {page}
+                     </Button>
+                  ))}
+               </Box>
+               {isLogin ? (
+                  <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
+                     <Typography
+                        variant="caption"
+                        sx={{
+                           mr: 5,
+                           fontSize: 15,
+                           fontWeight: 500,
+                        }}
+                     >
+                        Khóa học của tôi
+                     </Typography>
+                     <NotificationsActiveIcon />
+                  </Box>
+               ) : (
+                  <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
+                     <Button
+                        sx={{
+                           mb: 0,
+                           fontSize: 15,
+                           fontWeight: 500,
+                           backgroundColor: 'gray',
+                           borderRadius: 10,
+                        }}
+                     >
+                        Đăng nhập
+                     </Button>
+                  </Box>
+               )}
+            </Toolbar>
+         </Container>
       </CommonHeaderNav>
    );
 }
