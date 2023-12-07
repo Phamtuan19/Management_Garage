9c4b8fe080957a4fca91064c647f49c6b484c28a
Thuyngan <lanthuyngan@gmail.com>
2023-12-08 06:13:10 +0700

    feat: create personnel

--- src/@App/pages/Personnels/PersonnelCreate.tsx
+++ src/@App/pages/Personnels/PersonnelCreate.tsx
@@ -3,20 +3,44 @@ import BaseFormPersonnel from './components/BaseFormPersonnel';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { ValidationFormCreate, validationFormCreate } from './utils/personnel.schema';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation } from '@tanstack/react-query';
+import { errorMessage,successMessage } from '@Core/Helper/message';
+import { AxiosError} from 'axios';
+import { HandleErrorApi } from '@Core/Api/type';
+import setErrorMessageHookForm from '@App/helpers/setErrorMessageHookForm';
+import personnelService from '@App/services/personnel.service';
+import HttpStatusCode from '@Core/Configs/HttpStatusCode';
 
 const PersonnelCreate = () => {
-   const { handleSubmit, control } = useForm<ValidationFormCreate>({
+   const form = useForm<ValidationFormCreate>({
       resolver: yupResolver(validationFormCreate),
       defaultValues: validationFormCreate.getDefault(),
    });
+   const { mutate:handleCreatePersonnel,isLoading} = useMutation({
+      mutationFn: async(data:ValidationFormCreate)=>{
+         return await personnelService.create(data)
+      },
+      onSuccess:()=>{
+         successMessage('Thêm mới nhân viên thành công')
+      },
+       onError: (err: AxiosError) => {
+         const dataError = err.response?.data as HandleErrorApi;
 
-   const onSubmitForm: SubmitHandler<ValidationFormCreate> = (data) => {
-      console.log(data);
-   };
+         if (Number(dataError.statusCode) === Number(HttpStatusCode.BAD_REQUEST)) {
+            return setErrorMessageHookForm(form.setError, dataError.message);
+         }
+
+         return errorMessage(err);
+      },
+   })
+
+    
+   const onSubmitForm: SubmitHandler<ValidationFormCreate> = (data) =>handleCreatePersonnel(data)
+    
 
    return (
       <BaseBreadcrumbs arialabel="Personnels">
-         <BaseFormPersonnel handleSubmit={handleSubmit} onSubmitForm={onSubmitForm} control={control} />
+         <BaseFormPersonnel form={form} onSubmitForm={onSubmitForm} isLoading={isLoading} />
       </BaseBreadcrumbs>
    );
 };
