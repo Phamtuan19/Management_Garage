d78ceebd6c0c3e8b284c9cf71cdae20601b87f83
Thuyngan <lanthuyngan@gmail.com>
2023-12-12 00:16:44 +0700

    feat: Personnel-Update

---
+++ src/@App/pages/Personnels/PersonnelUpdate.tsx
@@ -0,0 +1,67 @@
+import BaseBreadcrumbs from "@App/component/customs/BaseBreadcrumbs";
+import BaseFormPersonnel from "./components/BaseFormPersonnel";
+import ROUTER_PATH from '@App/configs/router-path';
+import { ValidationFormCreate,validationFormCreate } from "./utils/personnel.schema";
+import { useParams } from "react-router-dom";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import personnelService from "@App/services/personnel.service";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { successMessage,errorMessage } from "@Core/Helper/message";
+import { HandleErrorApi } from "@Core/Api/type";
+import { AxiosError } from "axios";
+import HttpStatusCode from "@Core/Configs/HttpStatusCode";
+import setErrorMessageHookForm from "@App/helpers/setErrorMessageHookForm";
+const breadcrumbs = [
+    {
+       title: "Personnel",
+       link:ROUTER_PATH.PERSONNELS
+    }
+
+];
+const PersonnelUpdate = () => {
+    const { id: personnelId } = useParams();
+    const form = useForm<ValidationFormCreate>({
+        resolver:yupResolver(validationFormCreate),
+        defaultValues:validationFormCreate.getDefault(),
+
+    });
+
+    useQuery(
+        ['getPersonnel', personnelId],
+        async () => {
+            const res = await personnelService.find(personnelId!);
+            return res.data
+        },
+        {
+            onSuccess: (data) => {},
+        },
+    );
+    const { mutate: handleUpdatePersonnel, isLoading} = useMutation({
+        mutationFn: async (data:ValidationFormCreate) => {
+            return await personnelService.update(data, personnelId);
+        },
+        onSuccess: () => {
+            successMessage("Cập nhật nhân viên thành công!");
+        },
+         onError: (err: AxiosError) => {
+         const dataError = err.response?.data as HandleErrorApi;
+
+         if (Number(dataError.statusCode) === Number(HttpStatusCode.BAD_REQUEST)) {
+            return setErrorMessageHookForm(form.setError, dataError.message);
+         }
+
+         return errorMessage(err);
+      },
+
+    });
+    const onSubmitForm: SubmitHandler<ValidationFormCreate> = (data) => handleUpdatePersonnel(data);
+    return (
+        <BaseBreadcrumbs arialabel="Cập nhật" breadcrumbs={breadcrumbs}>
+             <BaseFormPersonnel form={form} onSubmitForm={onSubmitForm} isLoading={isLoading} />
+        </BaseBreadcrumbs>
+    )
+
+
+}
+export default PersonnelUpdate
