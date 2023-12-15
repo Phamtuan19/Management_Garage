41f2cc6cbacba5c736ab305cf0913cfb62d0000d
Tienltph19794 <tienltph19794@fpt.edu.vn>
2023-12-15 12:48:13 +0700

    feat: Create list user ui

---
+++ src/@App/pages/User/UserCreate.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+type Props = {}
+
+const UserCreate = (props: Props) => {
+  return (
+    <div>UserCreate</div>
+  )
+}
+export default UserCreate;
---
+++ src/@App/routes/rotue-action/user.tsx
@@ -0,0 +1,34 @@
+import Loadable from '../components/loadable';
+import ROUTE_PATH from '@App/configs/router-path';
+import PAGE_ACTION from '@App/configs/page-action';
+import { Outlet, RouteObject } from 'react-router-dom';
+import PermissionAccessRoute from '../components/PermissionAccessRoute';
+import MODULE_PAGE from '@App/configs/module-page';
+
+const UserCreate = Loadable('Users/UserCreate');
+const User = Loadable('Users');
+
+const userRoute: RouteObject = {
+   path: ROUTE_PATH.USER,
+   element: <Outlet />,
+   children: [
+      {
+         index: true,
+         element: (
+            <PermissionAccessRoute module={MODULE_PAGE.USER} action={PAGE_ACTION.VIEW} type="route">
+               <User />
+            </PermissionAccessRoute>
+         ),
+      },
+      {
+         path: ROUTE_PATH.USER + ROUTE_PATH.CREATE,
+         element: (
+            <PermissionAccessRoute module={MODULE_PAGE.USER} action={PAGE_ACTION.CREATE} type="route">
+               <UserCreate />
+            </PermissionAccessRoute>
+         ),
+      },
+   ],
+};
+
+export default userRoute;
