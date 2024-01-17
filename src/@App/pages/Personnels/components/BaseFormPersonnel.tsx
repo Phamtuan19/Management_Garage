870e19efc9da4262309f6cec64a8d16b59990b27
phamtuan19hd <phamtuan19hd@gmail.com>
2024-01-17 16:26:47 +0700

    add new personnel

--- src/@App/pages/Personnels/components/BaseFormPersonnel.tsx
+++ src/@App/pages/Personnels/components/BaseFormPersonnel.tsx
@@ -1,13 +1,16 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Control, FieldValues, SubmitHandler, UseFormReturn } from 'react-hook-form';
-import { FormControl, Grid, TextField } from '@mui/material';
+import { Box, FormControl, Grid, Stack } from '@mui/material';
 import ControllerLabel from '@Core/Component/Input/ControllerLabel';
 import ControllerTextField from '@Core/Component/Input/ControllerTextField';
 import ControllerSelect from '@Core/Component/Input/ControllerSelect';
 import { LoadingButton } from '@mui/lab';
 import ControllerRadioGroup from '@Core/Component/Input/ControllerRadioGroup';
+import ControllerTextFieldPassword from '@Core/Component/Input/ControllerTextFieldPassword';
 
 import { ValidationFormCreate } from '../utils/personnel.schema';
+import { GENDERS, POSITION } from '../utils/contants';
 
 interface BaseFormPersonnelPropType {
    form: UseFormReturn<ValidationFormCreate>;
@@ -16,122 +19,95 @@ interface BaseFormPersonnelPropType {
 }
 
 const BaseFormPersonnel = ({ form, onSubmitForm, isLoading }: BaseFormPersonnelPropType) => {
-   const { control, handleSubmit } = form;
+   const { control, handleSubmit, watch } = form;
    return (
       <form onSubmit={handleSubmit(onSubmitForm)}>
-         <Grid container spacing={2}>
-            <Grid item md={3}>
-               <ControllerLabel title="Full Name" required />
-               <ControllerTextField name="fullname" control={control} />
-            </Grid>
-            <Grid item md={3}>
-               <ControllerLabel title="Email" />
-               <ControllerTextField name="email" control={control} />
-            </Grid>
-            <Grid item md={3}>
-               <ControllerLabel title="Password" />
-               <ControllerTextField name="password" control={control} />
-            </Grid>
-            <Grid item xs={3}>
-               <TextField
-                  fullWidth
-                  type="file"
-                  // label="Chọn tệp tin"
-                  name="avatar"
-                  variant="outlined"
-                  // inputRef={control.register}
-               />
-            </Grid>
-            <Grid item md={3}>
-               <ControllerLabel title="Birthday" />
-               <ControllerTextField name="birthday" control={control} />
-            </Grid>
-            <Grid item md={3}>
-               <ControllerLabel title="Address" />
-               <ControllerTextField name="address" control={control} />
-            </Grid>
-            <Grid item md={3}>
-               <ControllerLabel title="Phone" />
-               <ControllerTextField name="phone" control={control} />
-            </Grid>
-            <Grid item md={3}>
-               <ControllerLabel title="CMND" />
-               <ControllerTextField name="cmnd" control={control} />
-            </Grid>
-            <Grid item md={3}>
-               <ControllerLabel title="Bộ phận" />
-               <ControllerSelect
-                  options={[
-                     { id: 1, lable: 'Bộ phận sửa chữa' },
-                     { id: 2, lable: 'Bộ phận Kho' },
-                  ]}
-                  valuePath="id"
-                  titlePath="lable"
-                  defaultValue=""
-                  name="role_id"
-                  control={control as unknown as Control<FieldValues>}
-               />
-            </Grid>
-            <Grid item md={3}>
-               <ControllerLabel title="Start_day" />
-               <ControllerTextField name="start_day" control={control} />
-            </Grid>
-            <Grid item md={3}>
-               <ControllerLabel title="End_day" />
-               <ControllerTextField name="end_day" control={control} />
-            </Grid>
-            <Grid item md={3}>
-               <ControllerLabel title="Bank number" />
-               <ControllerTextField name="bank_number" control={control} />
-            </Grid>
-            <Grid item md={3}>
-               <ControllerLabel title="Bank name" />
-               <ControllerTextField name="bank_name" control={control} />
-            </Grid>
-            <Grid item md={3}>
-               <ControllerLabel title="Bank account name" />
-               <ControllerTextField name="bank_account_name" control={control} />
-            </Grid>
+         <Stack position="relative" pb={6}>
+            <Grid container spacing={12} px={5} pt={3}>
+               <Grid item xs={6} md={5} display="flex" flexDirection="column" rowGap={2}>
+                  <Grid item>
+                     <ControllerLabel title="Account Name" required />
+                     <ControllerTextField name="account_name" control={control} placeholder="Tên tài khoản?" />
+                  </Grid>
+                  <Grid item>
+                     <ControllerLabel title="Full Name" required />
+                     <ControllerTextField name="full_name" control={control} placeholder="Họ và tên?" />
+                  </Grid>
+                  <Grid item>
+                     <ControllerLabel title="Password" required />
+                     <ControllerTextFieldPassword name="password" control={control} placeholder='Mật khẩu?'/>
+                  </Grid>
+                  <Grid item>
+                     <ControllerLabel title="Email" required />
+                     <ControllerTextField name="email" control={control} placeholder="Email?" />
+                  </Grid>
+                  <Grid item>
+                     <ControllerLabel title="Phone" required />
+                     <ControllerTextField name="phone" control={control} placeholder="Số điện thoại?" />
+                  </Grid>
+               </Grid>
 
-            <Grid item md={3}>
-               <FormControl>
-                  <ControllerLabel title="Giới tính" />
-                  <ControllerRadioGroup
-                     sx={{ display: 'flex', alignItems: 'center' }}
-                     name="gender"
-                     options={[
-                        { id: 'nam', title: 'Nam' },
-                        { id: 'nu', title: 'Nữ' },
-                     ]}
-                     valuePath="id"
-                     titlePath="title"
-                     control={control as unknown as Control<FieldValues>}
-                  />
-               </FormControl>
-            </Grid>
-            <Grid item md={3}>
-               <ControllerLabel title="Trạng thái" />
-               <ControllerRadioGroup
-                  name="status"
-                  sx={{ display: 'flex', alignItems: 'center' }}
-                  options={[
-                     { id: 'present', title: 'Có mặt' },
-                     { id: 'absent', title: 'Vắng mặt' },
-                  ]}
-                  valuePath="id"
-                  titlePath="title"
-                  control={control as unknown as Control<FieldValues>}
-               />
-            </Grid>
+               <Grid item xs={6} md={5} display="flex" flexDirection="column" rowGap={2}>
+                  <Grid item>
+                     <FormControl>
+                        <ControllerLabel title="Giới tính" required />
+                        <ControllerRadioGroup
+                           sx={{ display: 'flex' }}
+                           name="gender"
+                           options={GENDERS}
+                           valuePath="id"
+                           titlePath="title"
+                           control={control as unknown as Control<FieldValues>}
+                        />
+                     </FormControl>
+                  </Grid>
+                  <Grid item md={12} sx={{ flexBasis: { md: '0' } }}>
+                     <ControllerLabel title="Chức vụ" required />
+                     <ControllerSelect
+                        options={POSITION}
+                        valuePath="id"
+                        titlePath="lable"
+                        name="role_id"
+                        control={control as unknown as Control<FieldValues>}
+                     />
+                  </Grid>
 
-            <Grid item xs={12}>
+                  <Grid item>
+                     <ControllerLabel title="Link URL avatar" required />
+                     <ControllerTextField name="avatar" control={control} placeholder="Link địa chỉ hình ảnh?" />
+                  </Grid>
+                  <Grid>
+                     <Box
+                        sx={{
+                           display: 'flex',
+                           justifyContent: 'center',
+                           alignItems: 'center',
+                           p: '8px',
+                           width: '160px',
+                           height: '150px',
+                           border: 'solid 1px #ccc',
+                        }}
+                     >
+                        <img
+                           width="100%"
+                           height="100%"
+                           src={
+                              watch('avatar') ||
+                              'https://tse3.mm.bing.net/th?id=OIP.3IsXMskZyheEWqtE3Dr7JwHaGe&pid=Api&P=0&h=220'
+                           }
+                           alt="ảnh avatar"
+                        />
+                     </Box>
+                  </Grid>
+               </Grid>
+            </Grid>
+            <Box sx={{ position: 'absolute', top: 18, right: 30 }}>
                <LoadingButton type="submit" variant="contained" loading={isLoading}>
-                  Thêm
+                  Thêm Mới
                </LoadingButton>
-            </Grid>
-         </Grid>
+            </Box>
+         </Stack>
       </form>
    );
 };
-
 export default BaseFormPersonnel;
---
+++ src/@App/pages/Personnels/utils/contants.tsx
@@ -0,0 +1,8 @@
+export const GENDERS = [
+   { id: 'Nam', title: 'Nam' },
+   { id: 'Nữ', title: 'Nữ' },
+];
+export const POSITION = [
+   { id: '65958c90ba34c73e5e706c7b', lable: 'Quản lý' },
+   { id: '659ffdd143706da0003aa640', lable: 'Nhân viên' },
+];
