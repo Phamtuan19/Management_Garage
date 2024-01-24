c3564d4ff3b2b5b2b5d681261e37d750846b3a1c
Thuyngan <lanthuyngan@gmail.com>
2024-01-25 01:34:52 +0700

    feat: Create Distributor Details

--- src/@App/services/distributor.service.ts
+++ src/@App/services/distributor.service.ts
@@ -1,9 +1,30 @@
 import BaseService from '@Core/Api/BaseService';
 
+
 const distributorPath = {
    BASE: 'distributors',
 };
 
+interface Address {
+   code: number;
+   name: string;
+}
+export interface IDistributor {
+   _id: string;
+   code: string;
+   name: string;
+   phone: string;
+   email: string;
+   address: {
+      province: Address;
+      district: Address;
+      wards: Address;
+      specific: string;
+   };
+  
+   createdAt: string;
+   updatedAt: string;
+}
 class DistributorService extends BaseService {
    BASE_ENDPOINT = distributorPath.BASE;
 
