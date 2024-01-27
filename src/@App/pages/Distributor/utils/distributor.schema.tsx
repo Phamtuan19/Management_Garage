b1f9451bb65221864e86aa3af81f393c88400a45
Tienltph19794 <tienltph19794@fpt.edu.vn>
2024-01-27 21:37:14 +0700

    refactor: edit create and update distributor

--- src/@App/pages/Distributor/utils/distributor.schema.tsx
+++ src/@App/pages/Distributor/utils/distributor.schema.tsx
@@ -3,6 +3,7 @@ import messageValidate from '@App/helpers/messageValidate';
 import Regexs from '@Core/Configs/Regexs';
 import * as yup from 'yup';
 
+
 export const distributorSchema = yup.object({
    name: yup
       .string()
@@ -26,24 +27,24 @@ export const distributorSchema = yup.object({
       .matches(Regexs.phoneVn, messageValidate.format('Số điện thoại'))
       .default(''),
 
-   bank_account_number: yup
-      .string()
-      .trim()
-      .strict(true)
-      .test('custom_bank_number', messageValidate.format('Số tài khoản'), (value) => {
-         if (value!.length > 0 && !value?.match(Regexs.bankNumber)) {
-            return false;
-         }
+      bank_branch: yup.string().trim().strict(true).default(''),
 
-         return true;
-      })
-      .default(''),
+      bank_name: yup.string().trim().strict(true).default(''),
 
-   bank_branch: yup.string().trim().strict(true).default(''),
+      account_holder_name: yup.string().trim().strict(true).default(''),
 
-   bank_name: yup.string().trim().strict(true).default(''),
+      bank_account_number: yup
+         .string()
+         .trim()
+         .strict(true)
+         .test('custom_bank_number', messageValidate.format('Số tài khoản'), (value) => {
+            if (value!.length > 0 && !value?.match(Regexs.bankNumber)) {
+               return false;
+            }
 
-   account_holder_name: yup.string().trim().strict(true).default(''),
+            return true;
+         })
+         .default(''),
 
    address: yup.object({
 
@@ -63,7 +64,8 @@ export const distributorSchema = yup.object({
       }),
 
       specific: yup.string().trim().strict(true).default(''),
-   })
+   }),
+   bank_account_id: yup.object()
 });
 
 export type DistributorSchema = yup.InferType<typeof distributorSchema>;
