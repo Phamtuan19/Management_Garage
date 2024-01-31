6817dd8304827edfd37d290d20a35bac5e4b64c4
Tienltph19794 <tienltph19794@fpt.edu.vn>
2024-01-31 22:43:31 +0700

    refactor: router cars

---
+++ src/@App/routes/rotue-action/cars.tsx
@@ -0,0 +1,37 @@
+import ROUTE_PATH from '@App/configs/router-path';
+import { Outlet, RouteObject } from 'react-router-dom';
+import MODULE_PAGE from '@App/configs/module-page';
+import PAGE_ACTION from '@App/configs/page-action';
+
+import PermissionAccessRoute from '../components/PermissionAccessRoute';
+import Loadable from '../components/loadable';
+
+const Cars = Loadable('Cars');
+const CarsCreate = Loadable('Cars/CarsCreate');
+
+// const DistributorUpdate = Loadable('Distributor/DistributorUpdate');
+// const DistributorDetails = Loadable('Distributor/DistributorDetails');
+
+const carsRoute: RouteObject = {
+   path: ROUTE_PATH.CARS,
+   element: <Outlet />,
+   children: [
+      {
+         index: true,
+         element: (
+            <PermissionAccessRoute module={MODULE_PAGE.CARS} action={PAGE_ACTION.CREATE} type="route">
+               <Cars />
+            </PermissionAccessRoute>
+         ),
+      },
+      {
+         path: ROUTE_PATH.CARS + ROUTE_PATH.CREATE,
+         element: (
+            <PermissionAccessRoute module={MODULE_PAGE.CARS} action={PAGE_ACTION.CREATE} type="route">
+               <CarsCreate />
+            </PermissionAccessRoute>
+         ),
+      },
+   ],
+};
+export default carsRoute;
