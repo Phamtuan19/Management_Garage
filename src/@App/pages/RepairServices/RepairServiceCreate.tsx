ec296b6d03bbd4805896a56e5016d50c21785883
[ Thuy Milion ] <thuythph20667@fpt.edu.vn>
2024-02-01 17:54:22 +0700

    feat: create repair service

---
+++ src/@App/pages/RepairServices/RepairServiceCreate.tsx
@@ -0,0 +1,55 @@
+import BaseBreadcrumbs from '@App/component/customs/BaseBreadcrumbs';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation } from '@tanstack/react-query';
+import { errorMessage, successMessage } from '@Core/Helper/message';
+import { AxiosError } from 'axios';
+import setErrorMessageHookForm from '@App/helpers/setErrorMessageHookForm';
+import { HandleErrorApi } from '@Core/Api/axios-config';
+import ROUTE_PATH from '@App/configs/router-path';
+import repairServiceService from '@App/services/repairService.service';
+import { useNavigate } from 'react-router-dom';
+
+import { RepairServiceSchema, validationFormCreate } from './utils/repairService.schema';
+import BaseFormRepairService from './components/BaseFormRepairService';
+
+const breadcrumbs = [
+   {
+      title: 'Nhân viên',
+      link: ROUTE_PATH.REPAIR_SERVICES,
+   },
+];
+
+const RepairServiceCreate = () => {
+   const navigate = useNavigate();
+   const form = useForm<RepairServiceSchema>({
+      resolver: yupResolver(validationFormCreate),
+      defaultValues: validationFormCreate.getDefault(),
+   });
+   const { mutate: handleCreateRepairService, isLoading } = useMutation({
+      mutationFn: async (data: RepairServiceSchema) => {
+         return await repairServiceService.create(data);
+      },
+      onSuccess: () => {
+         successMessage('Thêm mới nhân viên thành công');
+         navigate('/fix/repair-services');
+      },
+      onError: (err: AxiosError) => {
+         const dataError = err.response?.data as HandleErrorApi;
+
+         setErrorMessageHookForm(form.setError, dataError.message);
+
+         return errorMessage(err);
+      },
+   });
+
+   const onSubmitForm: SubmitHandler<RepairServiceSchema> = (data) => handleCreateRepairService(data);
+
+   return (
+      <BaseBreadcrumbs arialabel="Thêm mới" breadcrumbs={breadcrumbs}>
+         <BaseFormRepairService form={form} onSubmitForm={onSubmitForm} isLoading={isLoading} />
+      </BaseBreadcrumbs>
+   );
+};
+
+export default RepairServiceCreate;
--- src/@App/services/personnel.service.ts
+++ src/@App/services/personnel.service.ts
@@ -3,6 +3,39 @@ import BaseService from '@Core/Api/BaseService';
 export const personnelPathUrl = {
    BASE: 'personnel',
 };
+export interface IPersonnel {
+   _id: string;
+   code: string;
+   fullName?: string;
+   email?: string;
+   accountName?: string;
+   phone?: string;
+   password?: string;
+   avatarUrl?: string;
+   birthDay?: Date;
+   hireDate?: Date; // Ngày nhận việc
+   leaveDate?: Date; // Ngày nghỉ làm
+   cccdNumber?: string; // Số CMND hoặc chứng minh nhân dân
+   gender?: string;
+   address?: {
+      province: {
+         code: number;
+         name: string;
+      };
+      district: {
+         code: number;
+         name: string;
+      };
+      wards: {
+         code: number;
+         name: string;
+      };
+      specific: string;
+   };
+   bankAccountId?: string;
+   roleId?: string; // Tên chức vụ - có thể là null nếu lưu dữ liệu khách hàng
+   isLock?: boolean;
+}
 
 class PersonnelService extends BaseService {
    BASE_ENDPOINT = personnelPathUrl.BASE;
