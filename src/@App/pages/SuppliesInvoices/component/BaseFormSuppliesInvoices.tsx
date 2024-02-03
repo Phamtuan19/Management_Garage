d0f9fed6d755457404dec489a523448bbb0e9e63
Phạm Anh Tuấn <phamtuan19hd@gmail.com>
2024-02-03 20:13:36 +0700

    refactor: controller autocomplete and add ArrowRight

--- src/@App/pages/SuppliesInvoices/component/BaseFormSuppliesInvoices.tsx
+++ src/@App/pages/SuppliesInvoices/component/BaseFormSuppliesInvoices.tsx
@@ -4,17 +4,45 @@ import { useEffect } from 'react';
 import { useAuth } from '@App/redux/slices/auth.slice';
 import personnelService from '@App/services/personnel.service';
 import ControllerLabel from '@Core/Component/Input/ControllerLabel';
-import { Box, Grid } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
-import { Control, FieldValues, UseFormReturn } from 'react-hook-form';
+import { UseFormReturn } from 'react-hook-form';
 import PageContent from '@App/component/customs/PageContent';
 import ControllerAutoComplate from '@Core/Component/Input/ControllerAutoComplate';
+import ArrowRight from '@App/component/common/ArrowRight';
 
 import { SuppliesInvoicesSchema } from '../utils/suppliesInvoices.schema';
 
 interface BaseFormSuppliesInvoicesPropType {
    form: UseFormReturn<SuppliesInvoicesSchema>;
 }
+
+const listArrowRight = [
+   {
+      title: 'Nháp',
+      name: 'draft',
+   },
+   {
+      title: 'Chờ phê duyệt',
+      name: 'waiting_approval',
+   },
+   {
+      title: 'Đã duyệt',
+      name: 'approved',
+   },
+   {
+      title: 'Đang tuyển',
+      name: 'recruiting',
+   },
+   {
+      title: 'Hoàn thành',
+      name: 'done',
+   },
+   {
+      title: 'Từ chối',
+      name: 'refused',
+   },
+];
 
 const BaseFormSuppliesInvoices = ({ form }: BaseFormSuppliesInvoicesPropType) => {
    const { control } = form;
@@ -32,13 +60,18 @@ const BaseFormSuppliesInvoices = ({ form }: BaseFormSuppliesInvoicesPropType) =>
    });
 
    useEffect(() => {
-      form.setValue('personnel_id', user?._id || '');
+      if (user?._id) {
+         form.setValue('personnel_id', user._id);
+      }
    }, [user?._id]);
 
    return (
       <>
          <Box component="form">
             <Grid container spacing={2}>
+               <Grid item xs={12}>
+                  <ArrowRight options={listArrowRight} check="refused" />
+               </Grid>
                <Grid item xs={12} md={9}>
                   <PageContent sx={{ mt: 0 }}>{/* <SuppliesInvoicesTable form={form} /> */}</PageContent>
                </Grid>
@@ -46,24 +79,38 @@ const BaseFormSuppliesInvoices = ({ form }: BaseFormSuppliesInvoicesPropType) =>
                   <PageContent sx={{ mt: 0 }}>
                      <Grid container spacing={2}>
                         <Grid item xs={12}>
-                           <Box height="75px">
+                           <Box height="">
                               <ControllerLabel title="Nhân viên tạo phiếu" required />
                               <ControllerAutoComplate
-                                 multiple
                                  options={personnels || []}
                                  valuePath="_id"
                                  titlePath="full_name"
                                  name="personnel_id"
-                                 control={control as unknown as Control<FieldValues>}
+                                 control={control}
                               />
                            </Box>
                         </Grid>
-                        {/* <Grid item xs={12}>
-                           <Box height="75px">
-                              <ControllerLabel title="Trạng thái" required />
-                              <TextField fullWidth value="" disabled />
+                        <Grid item xs={12}>
+                           <Box display="flex" justifyContent="space-between">
+                              <ControllerLabel title="Trạng thái:" />
+                              <Typography
+                                 sx={({ base }) => ({
+                                    color: base.text.gray2,
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    fontSize: 15,
+                                    padding: '5px 0',
+                                    fontWeight: 500,
+                                    gap: 0.5,
+                                    pt: 0,
+                                    pb: 0.5,
+                                    pl: 0.5,
+                                 })}
+                              >
+                                 Nháp
+                              </Typography>
                            </Box>
-                        </Grid> */}
+                        </Grid>
                      </Grid>
                   </PageContent>
                </Grid>
