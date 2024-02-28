d3c05ff08986db443d07de3afcfb32d7f5da30b5
Tienltph19794 <tienltph19794@fpt.edu.vn>
2024-02-28 23:02:07 +0700

    refactor: edit ui form create repair service

--- src/@App/pages/RepairServices/components/BaseFormRepairService.tsx
+++ src/@App/pages/RepairServices/components/BaseFormRepairService.tsx
@@ -1,10 +1,16 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable import/order */
 import { LoadingButton } from '@mui/lab';
-import { Box, Grid } from '@mui/material';
-import { SubmitHandler, UseFormReturn } from 'react-hook-form';
+import { Box, Grid, Typography } from '@mui/material';
+import { Controller, SubmitHandler, UseFormReturn } from 'react-hook-form';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import ControllerLabel from '@Core/Component/Input/ControllerLabel';
 import ControllerTextField from '@Core/Component/Input/ControllerTextField';
-
 import { RepairServiceSchema } from '../utils/repairService.schema';
 
 interface BaseFormRepairService {
@@ -16,30 +22,48 @@ interface BaseFormRepairService {
 
 const BaseFormRepairService = ({ form, isLoading, onSubmitForm, isUpdate }: BaseFormRepairService) => {
    const { handleSubmit, control } = form;
+
    return (
       <Box component="form" onSubmit={handleSubmit(onSubmitForm)}>
          <Grid container spacing={2}>
             <Grid item xs={12}>
                <Box height="96.5px">
                   <ControllerLabel title="Tên dịch vụ" required />
-                  <ControllerTextField name="name" control={control} placeholder="Tên dịch vụ ?" />
+                  <ControllerTextField name="name" control={control} placeholder="Tên dịch vụ " />
                </Box>
             </Grid>
             <Grid item xs={12}>
                <Box>
                   <ControllerLabel title="Giá" required />
-                  <ControllerTextField name="price" control={control} placeholder="Giá dịch vụ ?" />
+                  <ControllerTextField name="price" number control={control} placeholder="Giá dịch vụ " />
                </Box>
             </Grid>
             <Grid item xs={12}>
-               <ControllerLabel title="Giảm giá" required />
-               <ControllerTextField name="discount" control={control} placeholder="Giảm giá ?" />
+               <ControllerLabel title="Giảm giá (%)" required />
+               <ControllerTextField name="discount" number control={control} placeholder="Giảm giá " />
             </Grid>
             <Grid item xs={12}>
-               <ControllerLabel title="Mô tả" required />
-               <ControllerTextField name="describe" control={control} placeholder="Mô tả dịch vụ ?" />
-            </Grid>
+               <Controller
+                  name="describe"
+                  control={control}
+                  render={({ field }) => (
+                     <>
+                        <Typography variant="subtitle1" gutterBottom>
+                           Mô tả
+                        </Typography>
 
+                        <CKEditor
+                           editor={ClassicEditor}
+                           data={field.value}
+                           onChange={(_event, editor) => {
+                              const data = editor.getData();
+                              field.onChange(data);
+                           }}
+                        />
+                     </>
+                  )}
+               />
+            </Grid>
             <Grid item xs={12}>
                <LoadingButton type="submit" variant="contained" loading={isLoading}>
                   {isUpdate ? 'Cập nhật' : 'Thêm mới'}
