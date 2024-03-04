04e79e7005558a9e078f9f08b7f6f2822f9a978f
Tienltph19794 <tienltph19794@fpt.edu.vn>
2024-03-04 23:50:14 +0700

    refactor: fix form create and update  repair service

--- src/@App/pages/RepairServices/components/BaseFormRepairService.tsx
+++ src/@App/pages/RepairServices/components/BaseFormRepairService.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable import/order */
-import { Box, Grid, Typography } from '@mui/material';
-import { Controller, SubmitHandler, UseFormReturn } from 'react-hook-form';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Box, Grid } from '@mui/material';
+import { SubmitHandler, UseFormReturn } from 'react-hook-form';
+// import { CKEditor } from '@ckeditor/ckeditor5-react';
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import ControllerLabel from '@Core/Component/Input/ControllerLabel';
 import ControllerTextField from '@Core/Component/Input/ControllerTextField';
 import { RepairServiceSchema } from '../utils/repairService.schema';
@@ -35,35 +35,6 @@ const BaseFormRepairService = ({ form, onSubmitForm }: BaseFormRepairService) =>
             <Grid item xs={6} height="92px">
                <ControllerLabel title="Giảm giá (%)" required />
                <ControllerTextField name="discount" number control={control} placeholder="Giảm giá " />
-            </Grid>
-            <Grid item xs={12}>
-               <Controller
-                  name="describe"
-                  control={control}
-                  render={({ field }) => (
-                     <>
-                        <Typography variant="subtitle1" gutterBottom>
-                           Mô tả
-                        </Typography>
-                        <CKEditor
-                           editor={ClassicEditor}
-                           data={field.value}
-                           onReady={(editor) => {
-                              const rootElement = editor.editing.view.document.getRoot();
-                              if (rootElement !== null) {
-                                 editor.editing.view.change((writer) => {
-                                    writer.setStyle('height', '500px', rootElement);
-                                 });
-                              }
-                           }}
-                           onChange={(_event, editor) => {
-                              const data = editor.getData();
-                              field.onChange(data);
-                           }}
-                        />
-                     </>
-                  )}
-               />
             </Grid>
          </Grid>
       </Box>
