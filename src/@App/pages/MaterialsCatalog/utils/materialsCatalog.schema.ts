d6f67a3f09ab6e167571b2ce974622960126c9c5
Tienltph19794 <tienltph19794@fpt.edu.vn>
2024-03-05 02:45:47 +0700

    refactor: page materials catalog

--- src/@App/pages/MaterialsCatalog/utils/materialsCatalog.schema.ts
+++
@@ -1,9 +0,0 @@
-import messageValidate from '@App/helpers/messageValidate';
-import * as yup from 'yup';
-
-export const materialsCatalogSchema = yup.object({
-   name: yup.string().required(messageValidate.required('Tên danh mục')).strict(true).trim().default(''),
-   description: yup.string().strict(true).trim().default(''),
-});
-
-export type MaterialsCatalogSchema = yup.InferType<typeof materialsCatalogSchema>;
